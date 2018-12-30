2558a57ed05c7e44df21fcc3f358937084e5bf2e
anastiennagwa <46258656+anastiennagwa@users.noreply.github.com>
2018-12-30 11:12:35 +0200

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,19 @@
+#13 in cds
+w = [ 2.9, 2.4, 4.6, 4.1, 5]
+elementna = ['ca', 'na', 'si', 'mn', 'c']
+ev2j = 1.602*10**-19
+h = 6.626*10**-34
+c = 3*10**8 
+lamda = [ h*c/(ev2j*a) for a in w ]
+lamda1 = [ h*c*a/(ev2j) for a in w ]
+lamda2 = [ c*ev2j/a for a in w ]
+lamda3 = [ h*c/(ev2j) for a in w ]
+lamda4 = [ 2*3.14*h*c/(a*ev2j) for a in w ]
+
+for i in range(len(w)):
+    print( elementna[i], w[i])
+    print( "%.4E" %lamda[i])
+    print( "%.4E" %lamda1[i])
+    print( "%.4E" %lamda2[i])
+    print( "%.4E" %lamda3[i])
+    print( "%.4E" %lamda4[i])
