2dd4d9e81be6b4dc83e0c922de4535a6281e2ff7
anastiennagwa <46258656+anastiennagwa@users.noreply.github.com>
2019-01-01 08:38:28 +0200

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,3 +1,4 @@
+#n76, page 298 openstax vol3
 #32 in cds
 lamda = [600*10**-9, 350*10**-9, 700*10**-9,230*10**-9, 400*10**-9]
 h = 6.626*10**-34
@@ -10,8 +11,4 @@ p4 = [[(x+y)/2 for x,y in zip(p2,p1)]]
 
 for i in range(len(lamda)):
     print("%.4E" %lamda[i],"  ", "%.4E" %p[i], "%.4E" %p1[i], "%.4E" %p2[i])
-
-
-
-
 
