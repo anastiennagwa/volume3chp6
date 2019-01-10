9c9617739ed66f6b2750f43e2519559dff7a5131
anastiennagwa <46258656+anastiennagwa@users.noreply.github.com>
2019-01-10 10:45:52 +0200

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,23 @@
+#n156,p302
+v = [0.23, 0.5, 0.1, 0.66, 0.12]
+h = 6.626*10**-34
+c = 3*10**8 
+mp = 1.673*10**-27
+lamda = [ h/(x*c*mp) for x in v]
+lamda1 = [ h/(c*mp) for x in v]
+lamda2 = [ h/(2*3.14*x*c*mp) for x in v]
+lamda3 = [ h/(x*c) for x in v]
+lamda4 = [ h/(x*c*mp) for x in v]
+gamma = [1/(1-x**2)**0.5 for x in v]
+ke = [ y*0.5*mp*(x*c)**2 for y,x in zip (gamma,v)]
+ke1 = [ 0.5*mp*(x*c)**2 for x in v]
+ke2 = [ h*c/(x) for x in lamda]
+ke3 = [ 0.5*mp*c**2 for x in v]
+ke4 = [  h*c/(x) for x in lamda1]
+
+
+for i in range(len(v)):
+    print("%.4E" %v[i])
+    print("%.4E" %lamda[i], "%.4E" %lamda1[i], "%.4E" %lamda2[i], "%.4E" %lamda3[i], "%.4E" %lamda4[i])
+    print ("%.4E" %ke[i], "%.4E" %ke1[i],"%.4E" %ke2[i],"%.4E" %ke3[i],"%.4E" %ke4[i])
+    
